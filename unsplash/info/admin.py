8ca3f6993b87b1b92e6e40f796fa09a6e2e52777
toelapiut <toelapiut7@gmail.com>
2017-11-17 15:48:27 +0300

    added admin panel

--- unsplash/info/admin.py
+++ unsplash/info/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Editor,Photos,tags
+
+class PhotosAdmin(admin.ModelAdmin):
+    filter_horizontal=('tags',)
 
 # Register your models here.
+admin.site.register(Editor)
+admin.site.register(Photos,PhotosAdmin)
+admin.site.register(tags)
