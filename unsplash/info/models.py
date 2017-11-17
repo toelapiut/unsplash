9db73fd1aff95cfc0b90ef9441a8ad4b61deba70
toelapiut <toelapiut7@gmail.com>
2017-11-17 12:59:20 +0300

    added a model for editor

--- unsplash/info/models.py
+++ unsplash/info/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Editor(models.Model):
+    first_name=models.CharField(max_length=20)
+    last_name=models.CharField(max_length=20)
+    email=models.EmailField()
+    phone_number=models.CharField(max_length=14, blank=True)
+
+    def __str__(self):
+        return self.first_name
+    class Meta:
+        ordering=['first_name']
+
