5e670bf1ad1f296f103fd7922100c4dff3dbdfc0
toelapiut <toelapiut7@gmail.com>
2017-11-19 14:28:11 +0300

    added settings for deploying

--- unsplash/unsplash/settings.py
+++ unsplash/unsplash/settings.py
@@ -124,4 +124,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
