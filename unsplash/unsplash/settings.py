210c1ba19e4a6904b751f928f9efc0344fe30ea1
toelapiut <toelapiut7@gmail.com>
2017-11-19 16:19:28 +0300

    files

--- unsplash/unsplash/settings.py
+++ unsplash/unsplash/settings.py
@@ -123,13 +123,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_URL = '/static/'
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 
-STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/toel/Documents/python/django/unsplash/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
