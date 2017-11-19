57e7093c198072a03830e097390f5316c0dded12
toelapiut <toelapiut7@gmail.com>
2017-11-19 07:26:36 +0300

    added phot0_two/three in the admin panel

--- unsplash/info/admin.py
+++ unsplash/info/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Editor,Photos,tags
+from .models import Editor,Photos,tags,Photos_two,Photos_three
 
 class PhotosAdmin(admin.ModelAdmin):
     filter_horizontal=('tags',)
@@ -7,4 +7,6 @@ class PhotosAdmin(admin.ModelAdmin):
 # Register your models here.
 admin.site.register(Editor)
 admin.site.register(Photos,PhotosAdmin)
+admin.site.register(Photos_two,PhotosAdmin)
+admin.site.register(Photos_three,PhotosAdmin)
 admin.site.register(tags)
